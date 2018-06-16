3fe2a5947da3b5de5a17d4def2cb3d2b082b498b
Zillics <erik.zilliacus@gmail.com>
2018-06-16 21:56:00 +0300

    MIDI -> numpy matrix and numpy matrix -> MIDI now works in functions2.py

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 import glob
 import pickle
 import numpy as np
-from music21 import converter, instrument, note, chord, pitch, stream, duration
+from music21 import converter, instrument, note, chord, pitch, stream, duration, midi
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
@@ -9,57 +9,149 @@ from keras.layers import LSTM
 from keras.layers import Activation
 from keras.utils import np_utils
 from keras.callbacks import ModelCheckpoint
+import math
+
+#np.set_printoptions(threshold=1000)
+#WORRRRKS!!!!
+def testImport(filename,dest_filename):
+    mf = midi.MidiFile()
+    mf.open(filename)
+    mf.read()
+    mf.close()
+    print(len(mf.tracks))
+    stream = midi.translate.midiFileToStream(mf)
+    elements = stream.flat.notesAndRests
+    mf2 = midi.translate.streamToMidiFile(stream)
+    mf.open(dest_filename,'wb')
+    mf.write()
+    mf.close()
+
+def testImport2(filename,dest_filename):
+    mf = midi.MidiFile()
+    mf.open(filename)
+    mf.read()
+    mf.close()
+    s = midi.translate.midiFileToStream(mf)
+    notes = s.flat.notes
+    noteRests = s.flat.notesAndRests
+    elements = s.flat
+    prev_offset = -1
+    '''
+    for note_i in notes:
+        if isinstance(note_i,note.Note):
+            if(note_i.offset == prev_offset):
+                print("Offset: %f, pitch: %s, duration: %s !!!!!!!!!!!!!" % (note_i.offset,note_i.pitch,note_i.duration.type))
+            else:
+                print("Offset: %f, pitch: %s, duration: %s" % (note_i.offset,note_i.pitch,note_i.duration.type))
+            prev_offset = note_i.offset
+        else:
+            print(type(note))
+    '''
+    element_list = []
+    for element in elements:
+        element_list.append(element)
+    #print(element_list[0:10])
+    #print(type(element_list))
+    #midi_stream = stream.Stream(element_list)
+    #midi_stream.write('midi', fp=dest_filename)
+
+    print(element_list)
+
 
 def midiToPickle(filename):
     #MIDI -> music21 Score
     midi = converter.parse(filename) # music21.stream.Score
     partStream = midi.parts.stream() # music21.stream.Score
     #Music21 score -> Music21 RecursiveIterator
-    notes_to_parse = midi.flat.notes
-        
+    notes_to_parse = midi.flat
+    notes = []
+    for element in notes_to_parse:
+        notes.append(element)
+
+    '''
     #CREATE MATRIX
-    notes = np.zeros((n,2)) #notes matrix: [pitch_i,duration_i],[pitch_i+1,duration_i+1]....
+    n = 0
+    #Determine number of notes in RecursiveIterator
+    for note_i in notes_to_parse:
+        n = n + 1
+        pass
+    notes = np.zeros((n,3))
     i = 0
     prev_offset = 0 # offset of previous note
     prev_dur = 0 # duration of previous note
-    #Rests are expressed as pitch 0 (arbitrary, Change if needed)
+    # notes matrix: [pitch_i,duration_i, pause_i],[pitch_i+1,duration_i+1,pause_i+1]....
     for element in notes_to_parse:
-        if isinstance(element, note.Note): #Filter out everything but notes
+        if isinstance(element, note.Note): # Filter out everything but notes
             diff = element.offset - prev_offset # diff: difference between note offsets in quarterLengths (?)
-            if(diff - prev_dur > 0): # In other words: if there is a rest
-                notes[i,0] = 0 # Rest is marked as having pitch 0 (arbitrary, Change if needed)
-                notes[i,1] = diff - prev_dur # duration of rest = space between end of prev note and start of current note
-            else:
-                notes[i,0] = element.pitch.midi #Pitch as integer    
-                notes[i,1] = element.duration.quarterLength #Duration as float
-
+            #CURRENT PITCH
+            notes[i,0] = element.pitch.midi # Pitch as integer
+            #CURRENT DURATION
+            duration = element.duration.quarterLength
+            #if(duration == 0):
+                #duration = element.next().offset - float(element.offset) # Small value, but not maybe optimal??
+            notes[i,1] = duration #Duration as float in quarterLength
+            pause = abs(diff - prev_dur)
+            if(i > 0):
+                notes[i-1,2] = pause # Pause after the note
+            # Updating temp values to current values
+            prev_dur = duration
+            prev_offset = element.offset 
+            i = i + 1
     #Store Matrix in file
-    with open('data/tests/notes', 'wb') as filepath:
+    with open('data/notes', 'wb') as filepath:
         pickle.dump(notes, filepath)        
+    #print(notes[0:10,:])
+    '''
+    midi_stream = stream.Stream(notes)
+    midi_stream.write('midi', fp='aria_test9.mid')
 
     return 0
 
     
-def pickleToMidi(filename):
+def pickleToMidi(src_filename,dest_filename):
     #Open numpy matrix of notes + rests
-    with open(filename, 'rb') as filepath:
+    with open(src_filename, 'rb') as filepath:
         note_matrix = pickle.load(filepath)
-    prev_dur = 0 # temporary variable for determining offset of current note
-    prev_offset = 0 # temporary variable for determining offset of current note
-    note_list = []
 
+    n_notes = note_matrix.shape[0]
+    prev_offset = 0
+    prev_pause = 0
+    prev_dur = 0
+    note_list = []
     for i in range(0,n_notes):
         new_note = note.Note(int(note_matrix[i,0]))
-        new_note.offset = prev_dur + prev_offset
         new_note.duration.quarterLength = note_matrix[i,1]
-        new_note.storedInstrument = instrument.Piano()
+        new_note.storedInstrument = instrument.Piano() 
+        offset = float(prev_offset) + float(prev_dur) + float(prev_pause)
+        if(math.isclose(prev_offset,offset)):
+            offset = prev_offset + 0.1
+        new_note.offset = offset
+        #if(new_note.offset == prev_offset):
+        #    print("offset: %f , prev_offset: %f , new_note.offset: %f, condition: %s" % (offset,prev_offset,new_note.offset,math.isclose(prev_offset,offset)))
         note_list.append(new_note)
         # Update temporary values for next element
         prev_offset = new_note.offset
-        prev_dur = new_note.duration.quarterLength
+        prev_dur = note_matrix[i,1] #new_note.duration.quarterLength
+        prev_pause = note_matrix[i,2]
+
+    #print(note_matrix)
+    prev_offset = -1
+    for note_i in note_list:
+        print("Offset: %f, pitch: %s, duration: %s" % (note_i.offset,note_i.pitch,note_i.duration.type))
+        #if(prev_offset == note_i.offset):
+        #    print("Offset: %f, pitch: %s, duration: %s" % (note_i.offset,note_i.pitch,note_i.duration.type))
+        #prev_offset = note_i.offset
 
     midi_stream = stream.Stream(note_list)
-    midi_stream.write('midi', fp='test_aria3.mid')
+    midi_stream.write('midi', fp=dest_filename)
     
     return 0
 
+def midiToMidi(src_filename,dest_filename):
+    midiToPickle(src_filename)
+    #pickleToMidi('data/notes',dest_filename)
+    return 0
+
+if __name__ == '__main__':
+    #midiToMidi('aria_melody.mid','aria_test8.mid')
+    testImport2('aria_melody.mid','testWrite2.mid')
---
+++ functions2.py
@@ -0,0 +1,75 @@
+import pretty_midi
+import numpy as np
+
+def midiToMatrix(filename,sample_interval_ms):
+	midi_data = pretty_midi.PrettyMIDI(filename)
+	interval_s = sample_interval_ms*0.00002
+
+	piano_roll = midi_data.get_piano_roll(fs=sample_interval_ms, times=None)
+
+
+	time_steps = piano_roll.shape[1]
+	
+	out_matrix = np.zeros((time_steps,3))
+
+	t = 0
+	n_notes = piano_roll.shape[0]
+	i = 0 # counter for number of notes in out_matrix
+	rest = 0 # counter for rest before note
+	while(t < time_steps):
+		#Search for highest note which is activated
+		rest = rest + 1
+		k = n_notes - 1
+		#print(k)
+		while(k >= 0):
+			if(piano_roll[k,t] != 0):
+				# Storing the start position of the note
+				t0 = t
+				while(piano_roll[k,t] != 0):
+					#NOTE ON
+					t = t + 1
+					if(t >= time_steps):
+						#not_limit = False
+						break
+				# Insert note
+				out_matrix[i,0] = int(k) #PITCH
+				out_matrix[i,1] = (t - t0)*interval_s #DURATION
+				out_matrix[i,2] = rest*interval_s #REST BEFORE NOTE
+				#print(out_matrix[i,:])
+				i = i + 1
+				k = -1 # Jump out of second loop
+			k = k - 1
+			rest = 0
+		t = t + 1
+
+	out_matrix.resize(i + 1,3)
+	print(out_matrix[:,2])
+	return out_matrix
+
+def matrixToMidi(matrix,filename,interval_ms):
+	# Create a PrettyMIDI object
+	midi_obj = pretty_midi.PrettyMIDI()
+	# Create an Instrument instance for a Piano instrument
+	piano_program = pretty_midi.instrument_name_to_program('Acoustic Grand Piano')
+	piano = pretty_midi.Instrument(program=piano_program)
+	interval_s = interval_ms*0.00002
+
+	n_notes = matrix.shape[0]
+	t = 0.0
+	for i in range(0,n_notes):
+		p = int(matrix[i,0])
+		s = matrix[i,2] + t # Start = rest before note + current time
+		e = s + matrix[i,1] # End = start + duration of note
+		note = pretty_midi.Note(velocity=100, pitch=p, start=s, end=e)
+		#print("pitch: %d start: %f , end: %f" % (p,s,e))
+		piano.notes.append(note)
+		t = e
+
+	midi_obj.instruments.append(piano)
+	midi_obj.write(filename)
+
+
+if __name__ == '__main__':
+	matrix = midiToMatrix('aria_melody.mid',200)
+	print(matrix.shape)
+	matrixToMidi(matrix,'aria_pretty.mid',200)

77d5034b4f13baebc9b5232d5e90fb2681b5d0ad
Zillics <erik.zilliacus@gmail.com>
2018-06-26 23:05:10 +0300

    Fixed error: sample_interval_ms to sample_frequency. Now works independently of sample_frequency.

--- functions2.py
+++ functions2.py
@@ -1,11 +1,11 @@
 import pretty_midi
 import numpy as np
 
-def midiToMatrix(filename,sample_interval_ms):
+def midiToMatrix(filename,sample_frequency):
 	midi_data = pretty_midi.PrettyMIDI(filename)
-	interval_s = sample_interval_ms*0.00002
+	interval_s = 1/sample_frequency
 
-	piano_roll = midi_data.get_piano_roll(fs=sample_interval_ms, times=None)
+	piano_roll = midi_data.get_piano_roll(fs=sample_frequency, times=None)
 
 
 	time_steps = piano_roll.shape[1]
@@ -20,11 +20,11 @@ def midiToMatrix(filename,sample_interval_ms):
 		#Search for highest note which is activated
 		rest = rest + 1
 		k = n_notes - 1
-		#print(k)
 		while(k >= 0):
 			if(piano_roll[k,t] != 0):
 				# Storing the start position of the note
 				t0 = t
+				#Search for end of note
 				while(piano_roll[k,t] != 0):
 					#NOTE ON
 					t = t + 1
@@ -44,22 +44,22 @@ def midiToMatrix(filename,sample_interval_ms):
 
 	out_matrix.resize(i + 1,3)
 	print(out_matrix[:,2])
+	print(out_matrix[:,1])
+	print(out_matrix[:,0])
 	return out_matrix
 
-def matrixToMidi(matrix,filename,interval_ms):
+def matrixToMidi(matrix,filename):
 	# Create a PrettyMIDI object
 	midi_obj = pretty_midi.PrettyMIDI()
 	# Create an Instrument instance for a Piano instrument
 	piano_program = pretty_midi.instrument_name_to_program('Acoustic Grand Piano')
 	piano = pretty_midi.Instrument(program=piano_program)
-	interval_s = interval_ms*0.00002
-
 	n_notes = matrix.shape[0]
 	t = 0.0
 	for i in range(0,n_notes):
 		p = int(matrix[i,0])
 		s = matrix[i,2] + t # Start = rest before note + current time
-		e = s + matrix[i,1] # End = start + duration of note
+		e = s + matrix[i,1] # End = start + duration of note in seconds
 		note = pretty_midi.Note(velocity=100, pitch=p, start=s, end=e)
 		#print("pitch: %d start: %f , end: %f" % (p,s,e))
 		piano.notes.append(note)
@@ -70,6 +70,6 @@ def matrixToMidi(matrix,filename,interval_ms):
 
 
 if __name__ == '__main__':
-	matrix = midiToMatrix('aria_melody.mid',200)
+	matrix = midiToMatrix('midi-files/aria_melody.mid',500)
 	print(matrix.shape)
-	matrixToMidi(matrix,'aria_pretty.mid',200)
+	matrixToMidi(matrix,'midi-files/aria_pretty_500.mid')
